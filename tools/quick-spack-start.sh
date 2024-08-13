397e164b3c138085bf4082ebad493d3866a5c019
Eric Flumerfelt <eflumerf@fnal.gov>
2024-08-13 15:19:49 -0500

    Use install with --overwrite when gcc is force

--- tools/quick-spack-start.sh
+++ tools/quick-spack-start.sh
@@ -229,7 +229,7 @@ cd $Base
 BUILD_J=$((`cat /proc/cpuinfo|grep processor|tail -1|awk '{print $3}'` + 1))
 spack load gcc@13.1.0 >/dev/null 2>&1
 if [ $? -ne 0 ] || [ $opt_force_gcc -eq 1 ];then
-  spack install -j $BUILD_J gcc@13.1.0 $arch_opt +binutils
+  spack install --overwrite -j $BUILD_J gcc@13.1.0 $arch_opt +binutils
   installStatus=$?
   spack load gcc@13.1.0
 fi
