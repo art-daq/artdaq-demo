eaa1c937fb744f7025dae70d6d97b9fc4854548d
Eric Flumerfelt <eflumerf@fnal.gov>
2025-03-03 11:35:40 -0600

    cetmodules must still be added to environment

--- tools/quick-spack-start.sh
+++ tools/quick-spack-start.sh
@@ -319,6 +319,7 @@ if [[ ${opt_develop:-0} -eq 1 ]];then
 	# spack mpd new-project --force -y --name artdaq-develop -E artdaq-${demo_version} cxxstd=20 %gcc@13.1.0 generator=ninja # Upstream
 	spack mpd new-project --force -y --name artdaq-develop -E artdaq-${demo_version} cxxstd=20 %gcc@13.1.0 # Fork
 	spack env activate artdaq-develop
+	spack add cetmodules@3.26.00
 	spack concretize --force --deprecated
 	spack install
 	# spack mpd build # Upstream
