f5aeff875a3487e4859664a33887b31e7c02ab23
Eric Flumerfelt <eflumerf@fnal.gov>
2025-07-14 00:54:30 -0500

    Move to forks in art-daq

--- tools/quick-spack-start.sh
+++ tools/quick-spack-start.sh
@@ -170,7 +170,7 @@ source setup-env.sh
 if ! [ -d fermi-spack-tools ]; then
     #git clone https://github.com/FNALssi/fermi-spack-tools.git # Upstream
     #cd fermi-spack-tools && git checkout 965e0e73896328f8137c2bd53bad77a42b39e0bf; cd $Base
-    git clone https://github.com/eflumerf/fermi-spack-tools.git # Fork
+    git clone https://github.com/art-daq/fermi-spack-tools.git # Fork
     cd fermi-spack-tools && git checkout StableWithCairoFix; cd $Base
 else
     #cd fermi-spack-tools && git fetch -a && git checkout 965e0e73896328f8137c2bd53bad77a42b39e0bf ; cd $Base # Upstream
@@ -178,7 +178,7 @@ else
 fi
 if ! [ -d spack-mpd ]; then
     # git clone https://github.com/FNALssi/spack-mpd.git # Upstream
-    git clone https://github.com/eflumerf/spack-mpd.git # Fork
+    git clone https://github.com/art-daq/spack-mpd.git # Fork
 else
     cd spack-mpd && git pull && cd ..
 fi
@@ -393,7 +393,7 @@ alias rawEventDump="if [[ -n \\\$SETUP_TRACE ]]; then unsetup TRACE ; echo Disab
 alias mpd="spack mpd"
 # Note that the Ninja install command is needed to activate built changes!
 # alias mb="spack mpd build;pushd $Base/build;ninja install;popd" # When using upstream spack-mpd
-alias mb="spack mpd build -G Ninja;pushd $Base/build;ninja install;popd" # When using eflumerf fork
+alias mb="spack mpd build -G Ninja;pushd $Base/build;ninja install;popd" # When using art-daq fork
 
 if [ \${ARTDAQ_SETUP:-0} -eq 0 ]; then
   # Now save a copy of the environment after setup
